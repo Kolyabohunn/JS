9be0fd51716e254b05b349b5a45a0083ea2ab7b4
Kolyabohunn <kolyabohun89@gmail.com>
2021-05-20 11:38:33 +0100

    I forgot the lanes

--- homework-3_js/script.js
+++ homework-3_js/script.js
@@ -1,15 +1,15 @@
-// // 1.Подсчитать сумму всех чисел в заданном пользователем диапазоне.
-// let beginning = +prompt("Введiть початок дiапазону");
-// let theEnd = +prompt("Введiть кiнець дiапазону");
-// let sum = 0;
+// 1.Подсчитать сумму всех чисел в заданном пользователем диапазоне.
+let beginning = +prompt("Введiть початок дiапазону");
+let theEnd = +prompt("Введiть кiнець дiапазону");
+let sum = 0;
 
-// while (beginning < theEnd) {
-//   beginning++;
-//   alert(beginning);
-//   sum += beginning;
-//   console.log(sum);
-// }
-// alert(sum);
+while (beginning < theEnd) {
+  beginning++;
+  alert(beginning);
+  sum += beginning;
+  console.log(sum);
+}
+alert(sum);
 // 2.Запросить 2 числа и найти только наибольший общий делитель.
 const commonDivisor = +prompt(`задай 1 дiлитель `);
 const commonDivisor2 = +prompt(`задай 2 дiлитель `);
